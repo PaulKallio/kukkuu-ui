3b523213c16e92f769594b1b5ca78c36f7547f70
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2020-04-30 15:32:09 +0300

    Add the brochure PDFs and fix language codes

--- src/domain/home/moreInfo/constants/MoreInfoConstants.tsx
+++ src/domain/home/moreInfo/constants/MoreInfoConstants.tsx
@@ -4,24 +4,80 @@ export const moreInfoLinks: MoreInfoLink[] = [
   {
     langName: 'Français',
     langCode: 'fr',
-    url: 'fr',
+    url: `${process.env.PUBLIC_URL}/brochures/fr.pdf`,
   },
-  { langName: 'Русский', langCode: 'ru', url: 'ru' },
-  { langName: 'Español', langCode: 'es', url: 'es' },
+  {
+    langName: 'Русский',
+    langCode: 'ru',
+    url: `${process.env.PUBLIC_URL}/brochures/ru.pdf`,
+  },
+  {
+    langName: 'Español',
+    langCode: 'es',
+    url: `${process.env.PUBLIC_URL}/brochures/es.pdf`,
+  },
 
-  { langName: '日本語', langCode: 'ja', url: 'fixme' },
-  { langName: 'नेपाली गण', langCode: 'ne', url: 'fixme' },
-  { langName: 'Somali', langCode: 'so', url: 'fixme' },
+  {
+    langName: '中文',
+    langCode: 'zh',
+    url: `${process.env.PUBLIC_URL}/brochures/zh.pdf`,
+  },
+  {
+    langName: 'नेपाली गण',
+    langCode: 'npi',
+    url: `${process.env.PUBLIC_URL}/brochures/npi.pdf`,
+  },
+  {
+    langName: 'Somali',
+    langCode: 'som',
+    url: `${process.env.PUBLIC_URL}/brochures/som.pdf`,
+  },
 
-  { langName: 'زبان فارسی', langCode: 'fa', url: 'fixme' },
-  { langName: 'Eesti', langCode: 'et', url: 'fixme' },
-  { langName: 'Türkçe', langCode: 'tr', url: 'fixme' },
+  {
+    langName: 'زبان فارسی',
+    langCode: 'fas',
+    url: `${process.env.PUBLIC_URL}/brochures/fas.pdf`,
+  },
+  {
+    langName: 'Eesti',
+    langCode: 'et',
+    url: `${process.env.PUBLIC_URL}/brochures/et.pdf`,
+  },
+  {
+    langName: 'Türkçe',
+    langCode: 'tur',
+    url: `${process.env.PUBLIC_URL}/brochures/tur.pdf`,
+  },
 
-  { langName: 'اَلْعَرَبِيَّةُ', langCode: 'ar', url: 'fixme' },
-  { langName: 'كوردي', langCode: 'ku', url: 'uuu' },
-  { langName: 'Tiếng Việt', langCode: 'vi', url: 'aaa' },
+  {
+    langName: 'اَلْعَرَبِيَّةُ',
+    langCode: 'ar',
+    url: `${process.env.PUBLIC_URL}/brochures/ara.pdf`,
+  },
+  {
+    langName: 'کوردی',
+    langCode: 'ckb',
+    url: `${process.env.PUBLIC_URL}/brochures/ckb.pdf`,
+  },
+  {
+    langName: 'Tiếng Việt',
+    langCode: 'vi',
+    url: `${process.env.PUBLIC_URL}/brochures/vi.pdf`,
+  },
 
-  { langName: 'English', langCode: 'en', url: 'https://en' },
-  { langName: 'Svenska', langCode: 'sv', url: 'https://sv' },
-  { langName: 'Suomi', langCode: 'fi', url: 'https://fi' },
+  {
+    langName: 'English',
+    langCode: 'en',
+    url: `${process.env.PUBLIC_URL}/brochures/en.pdf`,
+  },
+  {
+    langName: 'Svenska',
+    langCode: 'sv',
+    url: `${process.env.PUBLIC_URL}/brochures/sv.pdf`,
+  },
+  {
+    langName: 'Suomi',
+    langCode: 'fi',
+    url: `${process.env.PUBLIC_URL}/brochures/fi.pdf`,
+  },
 ];
