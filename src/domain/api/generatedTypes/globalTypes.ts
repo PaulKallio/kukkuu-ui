c15bdd701c5c524b6b714271120003bbdff08184
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2020-05-28 16:10:01 +0300

    Update schema with email changes

--- src/domain/api/generatedTypes/globalTypes.ts
+++ src/domain/api/generatedTypes/globalTypes.ts
@@ -57,11 +57,11 @@ export interface EnrolOccurrenceMutationInput {
 }
 
 export interface GuardianInput {
-  email: string;
   firstName: string;
   lastName: string;
   phoneNumber?: string | null;
   language: Language;
+  email?: string | null;
 }
 
 export interface RelationshipInput {
