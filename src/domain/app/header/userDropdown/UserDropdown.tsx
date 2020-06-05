ae6abf69a5edf01197d02385e01c6864bfdd7a34
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2020-06-05 16:28:28 +0300

    Ensure that the childrenEventSelector is populated on auto-login

--- src/domain/app/header/userDropdown/UserDropdown.tsx
+++ src/domain/app/header/userDropdown/UserDropdown.tsx
@@ -1,16 +1,25 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { useMatomo } from '@datapunt/matomo-tracker-react';
+import { useQuery } from '@apollo/react-hooks';
 
+import { profileQuery as ProfileQueryType } from '../../../api/generatedTypes/profileQuery';
 import personIcon from '../../../../assets/icons/svg/person.svg';
 import Dropdown from '../../../../common/components/dropdown/Dropdown';
 import { isAuthenticatedSelector } from '../../../auth/state/AuthenticationSelectors';
-import { profileSelector } from '../../../profile/state/ProfileSelectors';
 import { loginTunnistamo, logoutTunnistamo } from '../../../auth/authenticate';
 import UserMenu from '../userMenu/UserMenu';
 import { flushAllState } from '../../../auth/state/AuthenticationUtils';
+import profileQuery from '../../../profile/queries/ProfileQuery';
+import LoadingSpinner from '../../../../common/components/spinner/LoadingSpinner';
+import { saveProfile } from '../../../profile/state/ProfileActions';
+import {
+  clearEvent,
+  saveChildrenEvents,
+} from '../../../event/state/EventActions';
+import { defaultProfileData } from '../../../profile/state/ProfileReducers';
 
 export interface UserDropdownProps {
   isSmallScreen?: boolean;
@@ -22,8 +31,24 @@ const UserDropdown: FunctionComponent<UserDropdownProps> = ({
   const { t } = useTranslation();
   const history = useHistory();
   const isAuthenticated = useSelector(isAuthenticatedSelector);
-  const profileData = useSelector(profileSelector);
+  const dispatch = useDispatch();
+
+  const { loading, error, data } = useQuery<ProfileQueryType>(profileQuery, {
+    skip: !isAuthenticated,
+  });
   const { trackEvent } = useMatomo();
+
+  useEffect(() => {
+    console.count('UserDropdown useEffect');
+    dispatch(saveProfile(data?.myProfile || defaultProfileData));
+    dispatch(clearEvent());
+    dispatch(saveChildrenEvents(data?.myProfile?.children || undefined));
+  }, [data, dispatch]);
+
+  if (loading) return <LoadingSpinner isLoading={true} />;
+  if (error) {
+    console.error(error);
+  }
 
   const logout = {
     label: t('authentication.logout.text'),
@@ -41,7 +66,9 @@ const UserDropdown: FunctionComponent<UserDropdownProps> = ({
 
   const user = {
     id: 'userButton',
-    label: profileData?.firstName || t('navbar.profileDropdown.profile.text'),
+    label: data?.myProfile?.firstName
+      ? data?.myProfile?.firstName
+      : t('navbar.profileDropdown.profile.text'),
     icon: personIcon,
   };
 
