de3d710997563dd1c7787b1e6672bb66761cdfeb
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-09-13 15:01:01 +0300

    Redirect away from event group page when no events

--- src/domain/eventGroup/EventGroupPage.tsx
+++ src/domain/eventGroup/EventGroupPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 
 import eventGroupQuery from './queries/eventGroupQuery';
 import EventGroup from './EventGroup';
@@ -12,10 +12,20 @@ type Params = {
 
 const EventGroupPage = () => {
   const { childId, eventGroupId } = useParams<Params>();
+  const history = useHistory();
   const query = useQuery(eventGroupQuery, {
     variables: {
       id: eventGroupId,
       childId,
+    },
+    onCompleted: (data) => {
+      const events = data?.eventGroup?.events?.edges ?? [];
+
+      // If there are no events the child can enrol into, assume that they have
+      // already enrolled and redirect them into their profile.
+      if (events.length === 0) {
+        history.replace('/profile');
+      }
     },
   });
 
