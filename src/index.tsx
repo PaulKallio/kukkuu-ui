b0fc9cf11f67bd4ca66d5be0e1e556e11425e5cc
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-10-20 12:57:01 +0300

    Upgrade @sentry/browser
    No breaking changes, but version 6.0.0 begins to send session data by
    default. To retain previous behaviour, I have toggled it off with a
    configuration.
    
    ref: https://github.com/getsentry/sentry-javascript/blob/753b71958ba09def4653f0ce32f619ce5c4e1e0b/CHANGELOG.md#600

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,7 @@ if (process.env.NODE_ENV === 'production') {
     dsn: process.env.REACT_APP_SENTRY_DSN,
     environment: process.env.REACT_APP_ENVIRONMENT,
     release: `${process.env.REACT_APP_APPLICATION_NAME}@${process.env.REACT_APP_VERSION}`,
+    autoSessionTracking: false,
   });
 }
 
