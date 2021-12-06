0928de3ceaeafad836b6eac5c5f99b6fe3ea450f
Tuomas Haapala <tuomas.haapala@anders.fi>
2021-12-06 23:17:25 +0200

    Fix browser tests
    There is no need to click login button because the user is redirected
    to Tunnistamo login page automatically.

--- browser-tests/utils/login.ts
+++ browser-tests/utils/login.ts
@@ -12,7 +12,6 @@ const givePermission = async (t: TestController) => {
 
 export const login = async (t: TestController) => {
   await t
-    .click(loginPage.loginButton)
     .click(ssoLogin.loginLink)
     .typeText(ssoLogin.username, testUsername())
     .typeText(ssoLogin.password, testUserPassword())
