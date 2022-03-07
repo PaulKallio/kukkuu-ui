d5fe046cf77c72ffea433e06eba780679aa08088
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-03-07 17:40:07 +0200

    Use new profile children list layout

--- src/domain/profile/children/ProfileChildrenList.tsx
+++ src/domain/profile/children/ProfileChildrenList.tsx
@@ -1,23 +1,24 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useMutation } from '@apollo/client';
 import { toast } from 'react-toastify';
 import * as Sentry from '@sentry/browser';
 import { useMatomo } from '@datapunt/matomo-tracker-react';
-import { IconPlusCircle } from 'hds-react';
+import { IconPlus } from 'hds-react';
 
 import { profileQuery_myProfile_children as Children } from '../../api/generatedTypes/profileQuery';
-import styles from './profileChildrenList.module.scss';
-import ProfileChild from './child/ProfileChild';
+import Button from '../../../common/components/button/Button';
+import LoadingSpinner from '../../../common/components/spinner/LoadingSpinner';
+import Text from '../../../common/components/text/Text';
 import AddNewChildFormModal from '../../registration/modal/AddNewChildFormModal';
 import { addChildMutation } from '../../child/mutation/ChildMutation';
 import { getSupportedChildData } from '../../child/ChildUtils';
-import LoadingSpinner from '../../../common/components/spinner/LoadingSpinner';
+import { UpdateChildMutationInput } from '../../api/generatedTypes/globalTypes';
 import profileQuery from '../queries/ProfileQuery';
 import useProfile, { ProfileQueryResult } from '../hooks/useProfile';
 import { getProjectsFromProfileQuery } from '../ProfileUtil';
-import { UpdateChildMutationInput } from '../../api/generatedTypes/globalTypes';
-import Button from '../../../common/components/button/Button';
+import ProfileChild from './child/ProfileChild';
+import styles from './profileChildrenList.module.scss';
 
 function getChildrenFromProfile({
   data,
@@ -40,38 +41,36 @@ const ProfileChildrenList = () => {
   if (mutationLoading) return <LoadingSpinner isLoading={true} />;
 
   return (
-    <>
-      <div className={styles.heading}>
-        <h2>{t('profile.heading')}</h2>
-        {isOpen && (
-          <AddNewChildFormModal
-            setIsOpen={setIsOpen}
-            addChild={(payload) => {
-              const supportedChildData: Omit<UpdateChildMutationInput, 'id'> =
-                getSupportedChildData(payload);
-              addChild({ variables: { input: supportedChildData } })
-                .then(() => {
-                  trackEvent({ category: 'action', action: 'Add child' });
-                })
-                .catch((error) => {
-                  toast.error(t('profile.addChildMutation.errorMessage'));
-                  Sentry.captureException(error);
-                });
-            }}
-          />
-        )}
-      </div>
-
-      <div className={styles.childrenList}>
-        {children?.edges ? (
-          <>
-            {getProjectsFromProfileQuery(children).map((project) => (
-              <Fragment key={project.id}>
-                <div className={styles.thisYearPartner}>
-                  <h3>
-                    {project.year} {project.name}
-                  </h3>
-                </div>
+    <section className={styles.layout}>
+      <Text variant="h2">{t('profile.heading')}</Text>
+      {isOpen && (
+        <AddNewChildFormModal
+          setIsOpen={setIsOpen}
+          addChild={(payload) => {
+            const supportedChildData: Omit<UpdateChildMutationInput, 'id'> =
+              getSupportedChildData(payload);
+            addChild({ variables: { input: supportedChildData } })
+              .then(() => {
+                trackEvent({ category: 'action', action: 'Add child' });
+              })
+              .catch((error) => {
+                toast.error(t('profile.addChildMutation.errorMessage'));
+                Sentry.captureException(error);
+              });
+          }}
+        />
+      )}
+      {children?.edges ? (
+        <ul className={styles.projects}>
+          {getProjectsFromProfileQuery(children).map((project) => (
+            <li key={project.id} className={styles.project}>
+              <Text as="h3" variant="body-xl">
+                {t('profile.message.projectDescription', {
+                  projectYear: project.year,
+                  projectName: project.name,
+                })}
+              </Text>
+              <ul className={styles.children}>
                 {children.edges.map((childEdge) => {
                   const child = childEdge?.node;
                   const childYear = childEdge?.node?.project.year;
@@ -81,28 +80,29 @@ const ProfileChildrenList = () => {
                     childYear &&
                     projectYear &&
                     childYear === projectYear ? (
-                    <ProfileChild key={child.id} child={child} />
+                    <li key={child.id}>
+                      <ProfileChild key={child.id} child={child} />
+                    </li>
                   ) : null;
                 })}
-              </Fragment>
-            ))}
-          </>
-        ) : (
-          <div className={styles.noChild}>
-            <p>{t('profile.children.noChild.text')}</p>
-          </div>
-        )}
-      </div>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className={styles.noChild}>
+          <p>{t('profile.children.noChild.text')}</p>
+        </div>
+      )}
       <Button
-        variant="supplementary"
+        variant="secondary"
         aria-label={t('child.form.modal.add.label')}
-        className={styles.addChild}
-        iconLeft={<IconPlusCircle />}
+        iconLeft={<IconPlus />}
         onClick={() => setIsOpen(true)}
       >
         {t('child.form.modal.add.label')}
       </Button>
-    </>
+    </section>
   );
 };
 
